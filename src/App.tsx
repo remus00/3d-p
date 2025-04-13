2c304243f5260c2a8924c6fcab401855d89a876c
remus00 <135813551+remus00@users.noreply.github.com>
2025-04-13 15:11:32 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <main>
+            <div className="">
+                <h1 className="text-3xl text-red-500">3d Portfolio</h1>
+            </div>
+        </main>
+    );
+};
+
+export default App;
